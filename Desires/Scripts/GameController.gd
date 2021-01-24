eadf6ad0cb15b8b7442bb861a21b2a7a569562a7
Renan Santana <renan_ota@hotmail.com>
2021-01-24 00:58:37 -0300

    Altera timer de pesca

--- Desires/Scripts/GameController.gd
+++ Desires/Scripts/GameController.gd
@@ -3,8 +3,8 @@ extends Node
 
 onready var vara = get_node("Vara")
 onready var boia = get_node("Boia")
-
 onready var pull_timer = $PullTime
+onready var next_fish_timer = $NextFishTimer
 
 enum states {
 	IDLE,
@@ -12,19 +12,20 @@ enum states {
 	CATCH
 }
 var actual_state
+const MIN_FISH_TIME = 5
+const MAX_FISH_TIME = 10
 
 
 func _ready():
 	randomize()
 	actual_state = states.IDLE
+	var next_fish = rand_range(MIN_FISH_TIME, MAX_FISH_TIME)
+	print(next_fish)
+	next_fish_timer.wait_time = next_fish
+	next_fish_timer.start()
 
 
 func _process(delta):
-	var chances = randf()
-	if chances < 0.01:
-		actual_state = states.ALERT
-		pull_timer.start()
-		print("!!!")
 	if Input.is_action_just_pressed("Pull"):
 		if actual_state == states.ALERT:
 			print("pegou!")
@@ -35,3 +36,13 @@ func _process(delta):
 
 func _on_PullTime_timeout():
 	actual_state = states.IDLE
+
+
+func _on_NextFishTimer_timeout():
+	actual_state = states.ALERT
+	pull_timer.start()
+	print("!!!")
+	var next_fish = rand_range(MIN_FISH_TIME, MAX_FISH_TIME)
+	print(next_fish)
+	next_fish_timer.wait_time = next_fish
+	next_fish_timer.start()
