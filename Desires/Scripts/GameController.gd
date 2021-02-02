4320dc8f38f1b66b2ef44426a84b234ffae91253
Renan Santana <renan_ota@hotmail.com>
2021-02-01 22:27:05 -0300

    Ajustes para vídeo

--- Desires/Scripts/GameController.gd
+++ Desires/Scripts/GameController.gd
@@ -13,7 +13,7 @@ enum states {
 }
 var actual_state
 const MIN_FISH_TIME = 5
-const MAX_FISH_TIME = 10
+const MAX_FISH_TIME = 15
 
 
 func _ready():
@@ -30,7 +30,13 @@ func _process(delta):
 	if Input.is_action_just_pressed("Pull"):
 		if actual_state == states.ALERT:
 			print("pegou!")
+			pull_timer.stop()
 			actual_state = states.IDLE
+			var next_fish = rand_range(MIN_FISH_TIME, MAX_FISH_TIME)
+			print("Próximo peixe em:")
+			print(next_fish)
+			next_fish_timer.wait_time = next_fish
+			next_fish_timer.start()
 		else:
 			print("...")
 
